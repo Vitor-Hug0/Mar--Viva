837b43287c1668df3252cea60764ee966a07e0ac
Vitor-Hug0 <vitorhugo.redes6@gmail.com>
2025-04-14 23:26:54 -0300

    aaaa

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
-$hostname = "localhost";//definindo o host
-$database = "mareviva";//definindo o nome do banco de dados
-$user = "root";//nome do usuario
-$password = "";//senha
+$hostname = 'localhost';//definindo o host
+$database = 'mareviva';//definindo o nome do banco de dados
+$user = 'root';//nome do usuario
+$password = '';//senha
 
 $conexion = new mysqli($hostname, $user, $password, $database);
 
--- php/registertest.php
+++ php/registertest.php
@@ -1,18 +1,16 @@
 <?php
- $email = $_POST['email'];//verificando o tipo email
- $senha = $_POST['password'];
+ $nome = $_POST['nome'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+ $data = $_POST['data'];
  $cpf = $_POST['cpf'];
- $datanascimento = $_POST['data_nascimento'];
- $rua = $_POST['rua'];
  $cep = $_POST['cep'];
- $complemento = $_POST['complemento'];
  $cidade = $_POST['cidade'];
- $fullname =$_POST['fullname'];
+ $rua = $_POST['rua'];
+ $complemento = $_POST['complemento'];
 
  include_once('connection.php');
  if (isset($_POST['submit'])){
- $result = mysqli_query($conexion, "INSERT INTO usuarios (gmail, senha, datadenascimento, cpf, cep, cidade, rua, complemento) VALUES ('$email','$senha','$datanascimento','$cpf','$cep','$cidade','$rua','$complemento')");
- header("Location: login.html");
- exit();
+ $result = mysqli_query($conexion, "INSERT INTO usuarios (gmail, senha, datadenascimento,cpf,cep,cidade,rua,complemento) VALUES ('$email','$password','$data','$cpf','$cep','$cidade','$rua','$complemento')");
  }
 ?>
